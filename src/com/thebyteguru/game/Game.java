6e5653350c1ee8ffcba58da85a60025093b75bea
solomon202 <vivo5757@yandex.ru>
2026-07-02 23:20:55 +0300

    first commit

---
+++ src/com/thebyteguru/game/Game.java
@@ -0,0 +1,152 @@
+package com.thebyteguru.game;
+
+import java.awt.Graphics2D;
+
+import com.tank.level.Level;
+import com.thebyteguru.IO.Input;
+import com.thebyteguru.display.Display;
+import com.thebyteguru.graphics.TextureAtlas;
+import com.thebyteguru.utils.Time;
+// класс реализовывает интерфейс и его методы run поток выполняет несколько задачь одновременно 
+public class Game implements Runnable {
+//у игры есть размер название 
+	public static final int		WIDTH			= 800;
+	public static final int		HEIGHT			= 600;
+	public static final String	TITLE			= "Tanks";
+	public static final int		CLEAR_COLOR		= 0xff000000;
+	public static final int		NUM_BUFFERS		= 3;
+//скорость обновления 
+	public static final float	UPDATE_RATE		= 60.0f;
+	public static final float	UPDATE_INTERVAL	= Time.SECOND / UPDATE_RATE;
+	public static final long	IDLE_TIME		= 1;
+  //картинки 
+	public static final String	ATLAS_FILE_NAME	= "texture_atlas.png";
+  //теперь обьекты которые нужны в игре 
+	private boolean				running;
+	private Thread				gameThread;
+	private Graphics2D			graphics;
+	private Input				input;
+	private TextureAtlas		atlas;
+	private Player				player;
+	private static int								stage			= 1;
+	private static Level							lvl;
+//вызываем обьект экран 
+	public Game() {
+		running = false;
+		//обстрактный класс не создается экземпляр 
+		Display.create(WIDTH, HEIGHT, TITLE,CLEAR_COLOR, NUM_BUFFERS);
+		graphics = Display.getGraphics();
+		//создать классс
+		//создать экземпляр
+		//создаем отдельные параметры для методов
+		input = new Input();
+		Display.addInputListener(input);
+		atlas = new TextureAtlas(ATLAS_FILE_NAME);
+		player = new Player(300, 300, 2, 3, atlas);
+		lvl = new Level(atlas, stage);
+	}
+ //запуск потока 
+	//далее метод run
+	public synchronized void start() {
+// Меняем статус
+		if (running)
+			return;
+
+		running = true;
+		gameThread = new Thread(this);
+		gameThread.start();
+
+	}
+
+	public synchronized void stop() {
+
+		if (!running)
+			return;
+
+		running = false;
+
+		try {
+			gameThread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		cleanUp();
+
+	}
+
+	private void update() {
+		player.update(input);
+	}
+
+	private void render() {
+	    //заливаем цветом очистить
+		Display.clear();
+		
+		lvl.render(graphics);
+		
+		//рисуем 
+    	player.render(graphics);
+    	//отображаем
+		Display.swapBuffers();
+	}
+
+	public void run() {
+
+		int fps = 0;
+		int upd = 0;
+		int updl = 0;
+
+		long count = 0;
+
+		float delta = 0;
+
+		long lastTime = Time.get();
+		while (running) {
+			long now = Time.get();
+			long elapsedTime = now - lastTime;
+			lastTime = now;
+
+			count += elapsedTime;
+
+			boolean render = false;
+			delta += (elapsedTime / UPDATE_INTERVAL);
+			while (delta > 1) {
+				update();
+				upd++;
+				delta--;
+				if (render) {
+					updl++;
+				} else {
+					render = true;
+				}
+			}
+
+			if (render) {
+				render();
+				fps++;
+			} else {
+				try {
+					Thread.sleep(IDLE_TIME);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+
+			if (count >= Time.SECOND) {
+				Display.setTitle(TITLE + " || Fps: " + fps + " | Upd: " + upd + " | Updl: " + updl);
+				upd = 0;
+				fps = 0;
+				updl = 0;
+				count = 0;
+			}
+
+		}
+
+	}
+
+	private void cleanUp() {
+		Display.destroy();
+	}
+
+}
---
+++ src/com/thebyteguru/main/Main.java
@@ -0,0 +1,14 @@
+package com.thebyteguru.main;
+
+import com.thebyteguru.game.Game;
+
+public class Main {
+ //класс запуска 
+	public static void main(String[] args) {
+//создает обьект игра
+		Game tanks = new Game();
+		//у класса ира есть метод адрес в пакете и метод .
+		tanks.start();
+
+	}
+}
